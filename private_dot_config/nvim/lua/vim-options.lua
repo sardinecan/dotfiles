6c76f5db11a6dd4ed15d6b1a0ba1adfc677204f1
sardinecan <morvan.josselin@gmail.com>
2024-07-10 20:02:23 +0200

    Update .config/nvim/lua/plugins/lsp-config.lua Update .config/nvim/lua/vim-options.lua

--- private_dot_config/nvim/lua/vim-options.lua
+++ private_dot_config/nvim/lua/vim-options.lua
@@ -8,7 +8,7 @@ vim.g.background = "light"
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+--vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
