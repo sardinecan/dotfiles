d73b4b2f91c33cf972258e2763fd3812b3394ac2
sardinecan <morvan.josselin@gmail.com>
2024-07-13 21:07:58 +0200

    added capabilities to julials

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -40,6 +40,7 @@ return {
       })
 
       lspconfig.julials.setup({
+        capabilities=capabilities,
         on_new_config = function(new_config, _)
           local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
           if require'lspconfig'.util.path.is_file(julia) then
