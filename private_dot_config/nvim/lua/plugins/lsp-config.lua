bbe46737108b57a403a6706f9d0cfeae9d97978f
sardinecan <morvan.josselin@gmail.com>
2024-07-14 11:59:32 +0200

    added shortcuts for lsp

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -53,9 +53,21 @@ return {
 
 
       --vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+
+      --local opts = { buffer = ev.buf, silent = true }
+
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      --vim.keymap.set("n", "<leader>gI", vim.lsp.lsp_implementations, {})
+      --vim.keymap.set("n", "<leader>gt", vim.lsp.lsp_type_definitions, {})
+      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", {}) -- show  diagnostics for file
+      vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", {}) -- mapping to restart lsp if necessary
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {}) -- show diagnostics for line
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {}) -- jump to previous diagnostic in buffer
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {}) -- jump to next diagnostic in buffer
     end,
   },
 }
--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -8,8 +8,9 @@ return {
   },
   config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {}) -- open neotree panel
-		vim.keymap.set("n", "<C-N>", ":Neotree close<CR>", {}) -- close neotree panel
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {}) -- see opened files 
-	end,
+		vim.keymap.set("n", "<leader>q", ":Neotree close<CR>", {}) -- close neotree panel
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {}) -- see opened files 	
+    vim.keymap.set("n", "<leader>gst", ":Neotree buffers reveal float<CR>", {}) -- see opened files 
+  end,
 }
 
