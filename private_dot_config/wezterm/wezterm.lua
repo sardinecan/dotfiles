17c5c15b414344c44c1b1d39958648c8d0fd0bac
sardinecan <morvan.josselin@gmail.com>
2024-08-27 18:32:48 +0200

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -41,7 +41,81 @@ config = {
     "Symbols Nerd Font Mono",
   }),
 
-  font_size = 16
+  font_size = 16,
+
+  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 },
+  keys = {
+    {
+      key = '|',
+      mods = 'LEADER',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      mods = "LEADER",
+      key = "x",
+      action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+      mods = "LEADER",
+      key = "b",
+      action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+      mods = "LEADER",
+      key = "n",
+      action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+      mods = "LEADER",
+      key = "|",
+      action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+      mods = "LEADER",
+      key = "-",
+      action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    {
+      mods = "LEADER",
+      key = "h",
+      action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+      mods = "LEADER",
+      key = "j",
+      action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+      mods = "LEADER",
+      key = "k",
+      action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+      mods = "LEADER",
+      key = "l",
+      action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+      mods = "LEADER",
+      key = "LeftArrow",
+      action = wezterm.action.AdjustPaneSize { "Left", 5 }
+    },
+    {
+      mods = "LEADER",
+      key = "RightArrow",
+      action = wezterm.action.AdjustPaneSize { "Right", 5 }
+    },
+    {
+      mods = "LEADER",
+      key = "DownArrow",
+      action = wezterm.action.AdjustPaneSize { "Down", 5 }
+    },
+    {
+      mods = "LEADER",
+      key = "UpArrow",
+      action = wezterm.action.AdjustPaneSize { "Up", 5 }
+    },
+  }
 }
 
 return config
