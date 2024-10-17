c79ccc681bcfb25a95e30898d06ec61982f1d32b
sardinecan <morvan.josselin@gmail.com>
2024-10-17 22:18:53 +0200

    Add .julia/config/startup.jl

---
+++ dot_julia/config/startup.jl
@@ -0,0 +1,26 @@
+println("Salutations !")
+# ------------------------------
+# Activating env automatically
+# ------------------------------
+using Pkg
+if isfile("Project.toml") && isfile("Manifest.toml")
+    Pkg.activate(".")
+end
+
+# ------------------------------
+# utility fonctions
+# ------------------------------
+function up()
+  println("Updating all packages")
+  Pkg.update()
+end
+
+function add(pkgname::String)
+  println("Adding package $pkgname")
+  Pkg.add(pkgname)
+end
+
+function rm(pkgname::String)
+  println("Removing package $pkgname")
+  Pkg.rm(pkgname)
+end
