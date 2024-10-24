90c517979339443e88185685d407d489adadb1f9
sardinecan <morvan.josselin@gmail.com>
2024-10-24 22:03:52 +0200

    added highlight to special spaces

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -24,6 +24,19 @@ vim.keymap.set('v', '<leader>y', '"+y', {})
 vim.opt.spell = true
 vim.opt.spelllang = {"fr", "en"}
 
+local function highlight_special_spaces()
+  vim.api.nvim_command('highlight Nbsp guibg=lightgrey guifg=magenta')
+  vim.api.nvim_command('highlight TrailingSpace guibg=lightgrey guifg=magenta')
+
+  vim.fn.matchadd("Nbsp", [[\%u00A0]])
+  vim.fn.matchadd("TrailingSpace", [[\s\+$]])
+end
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = {"*"},
+  callback = highlight_special_spaces
+})
+
 vim.o.list = true
 vim.o.listchars = 'trail:·,nbsp:⎵'
 
