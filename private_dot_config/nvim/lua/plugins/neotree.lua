efafe74fed3611c40d49f13750929364d6dec24f
sardinecan <morvan.josselin@gmail.com>
2024-11-16 18:33:33 +0100

    reviewed nvim shortcuts

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -7,7 +7,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {}) -- open neotree panel
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {}) -- open neotree panel
 		vim.keymap.set("n", "<leader>q", ":Neotree close<CR>", {}) -- close neotree panel
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {}) -- see opened files 	
     vim.keymap.set("n", "<leader>gst", ":Neotree git_status reveal float<CR>", {}) -- see git status files 
