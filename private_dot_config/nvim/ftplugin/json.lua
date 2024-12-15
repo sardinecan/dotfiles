26c09f3b70ce70f6166062039bb45020dd2e161e
sardinecan <morvan.josselin@gmail.com>
2024-12-15 15:18:16 +0100

    Add .config/nvim/ftplugin/json.lua

---
+++ private_dot_config/nvim/ftplugin/json.lua
@@ -0,0 +1,5 @@
+-- réglages pour l'indentation des fichiers JSON
+vim.bo.tabstop = 4      -- 1 tab = 4 espaces
+vim.bo.shiftwidth = 4   -- 4 espaces pour l'indentation automatique
+vim.bo.expandtab = true -- convertir les tabs en espaces
+
