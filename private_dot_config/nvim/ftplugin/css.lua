2c425740ed07cb4971e3c9be16a548bcccef14a4
sardinecan <morvan.josselin@gmail.com>
2024-12-15 15:18:07 +0100

    Add .config/nvim/ftplugin/css.lua

---
+++ private_dot_config/nvim/ftplugin/css.lua
@@ -0,0 +1,5 @@
+-- réglages pour l'indentation des fichiers CSS
+vim.bo.tabstop = 2      -- 1 tab = 2 espaces
+vim.bo.shiftwidth = 2   -- 2 espaces pour l'indentation automatique
+vim.bo.expandtab = true -- convertir les tabs en espaces
+
