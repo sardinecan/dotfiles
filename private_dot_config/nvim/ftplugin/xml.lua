694671328eb664ff54cd9dd9add0b9ef6e7bc4ba
sardinecan <morvan.josselin@gmail.com>
2024-12-15 13:43:22 +0100

    Add .config/nvim/ftplugin/julia.lua Add .config/nvim/ftplugin/lua.lua Add .config/nvim/ftplugin/xml.lua

---
+++ private_dot_config/nvim/ftplugin/xml.lua
@@ -0,0 +1,9 @@
+-- réglages pour l'indentation des fichiers XML
+vim.bo.smartindent = false
+vim.bo.autoindent = false
+vim.cmd([[TSDisable indent]])
+
+vim.bo.tabstop = 4      -- 1 tab = 4 espaces
+vim.bo.shiftwidth = 4   -- 4 espaces pour l'indentation automatique
+vim.bo.expandtab = true -- convertir les tabs en espaces
+
