1f97dacb7f1d3c8cd96751c2a86ab9481d85637f
sardinecan <morvan.josselin@gmail.com>
2024-12-16 14:03:28 +0100

    Update .config/nvim/ftplugin/css.lua

--- private_dot_config/nvim/ftplugin/css.lua
+++ private_dot_config/nvim/ftplugin/css.lua
@@ -1,4 +1,4 @@
--- réglages pour l'indentation des fichiers CSS
+-- réglages pour l'indentation des fichiers XQuery
 vim.bo.tabstop = 2      -- 1 tab = 2 espaces
 vim.bo.shiftwidth = 2   -- 2 espaces pour l'indentation automatique
 vim.bo.expandtab = true -- convertir les tabs en espaces
