915d280d740432da3076669534369da3771fa28d
sardinecan <morvan.josselin@gmail.com>
2024-12-16 14:02:33 +0100

    Update .config/nvim/lua/vim-options.lua

--- private_dot_config/nvim/lua/vim-options.lua
+++ private_dot_config/nvim/lua/vim-options.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set statusline+=%y")
+vim.cmd("set statusline+=%y%f")
 vim.g.mapleader = " "
 vim.g.background = "light"
 
