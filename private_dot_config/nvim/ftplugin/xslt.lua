e708af3db212dd53f40bd6c0d9324384827302ba
sardinecan <morvan.josselin@gmail.com>
2024-12-16 14:02:15 +0100

    Add .config/nvim/ftplugin/xslt.lua

---
+++ private_dot_config/nvim/ftplugin/xslt.lua
@@ -0,0 +1,10 @@
+-- réglages pour l'indentation des fichiers XSLT
+vim.bo.smartindent = false
+vim.bo.autoindent = false
+vim.cmd([[filetype indent off]]) 
+vim.cmd([[TSDisable indent]])
+
+vim.bo.tabstop = 4      -- 1 tab = 4 espaces
+vim.bo.shiftwidth = 4   -- 4 espaces pour l'indentation automatique
+vim.bo.expandtab = true -- convertir les tabs en espaces
+
