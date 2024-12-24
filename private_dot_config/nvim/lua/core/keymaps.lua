817b38551d3f267a1178a7469f4548c7e92368ab
sardinecan <morvan.josselin@gmail.com>
2024-12-24 17:00:51 +0100

    reviewed files content

--- private_dot_config/nvim/lua/core/keymaps.lua
+++ private_dot_config/nvim/lua/core/keymaps.lua
@@ -1,1 +1,9 @@
+
+-- nvim navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- copy to clipboard
 vim.keymap.set('v', '<leader>y', '"+y', {})
--- private_dot_config/nvim/lua/core/options.lua
+++ private_dot_config/nvim/lua/core/options.lua
@@ -8,11 +8,11 @@ vim.g.background = "light"
 
 vim.opt.swapfile = false
 
+vim.opt.spell = true
+vim.opt.spelllang = {"fr", "en"}
+
+vim.o.smartindent = false
 -- Navigate vim panes better
---vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
