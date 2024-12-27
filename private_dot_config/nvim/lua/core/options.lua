6dcc67e4795fb230887e216fc79578ce43db108a
sardinecan <morvan.josselin@gmail.com>
2024-12-27 15:20:26 +0100

    Update .config/nvim/lua/core/options.lua

--- private_dot_config/nvim/lua/core/options.lua
+++ private_dot_config/nvim/lua/core/options.lua
@@ -13,6 +13,19 @@ vim.opt.spelllang = { "fr", "en" }
 
 vim.opt.smartindent = false
 
+vim.opt.foldmethod = 'indent'
+vim.opt.foldlevel = 99
+vim.opt.foldenable = true
+
 vim.wo.number = true
 
 vim.g.mapleader = " "
+
+vim.cmd("filetype plugin on")
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "tex",
+    callback = function()
+        vim.bo.syntax = "on" -- Utilise la syntaxe Vim classique
+    end,
+})
+
