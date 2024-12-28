7b3b6ca4871dd490d65685d358f5fcef69b3f4f9
sardinecan <morvan.josselin@gmail.com>
2024-12-28 19:52:52 +0100

    Update .config/nvim/lua/plugins/onedarkpro.lua

--- private_dot_config/nvim/lua/plugins/onedarkpro.lua
+++ private_dot_config/nvim/lua/plugins/onedarkpro.lua
@@ -1,5 +1,11 @@
 return  {
   "olimorris/onedarkpro.nvim",
   priority = 1000, -- Ensure it loads first
+  opts = {
+    options = {
+      transparency = true,
+      highlight_inactive_windows = true
+    }
+  }
 }
 
