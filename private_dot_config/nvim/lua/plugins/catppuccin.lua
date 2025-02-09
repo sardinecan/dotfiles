35c104d62432e2ac6db3b066a0ee77eaceb245ec
Josselin Morvan <morvan.josselin@gmail.com>
2025-02-09 19:53:57 +0100

    reviewed catppucin flavour

--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
       flavour = "auto", -- latte, frappe, macchiato, mocha
       background = {
         light = "latte",
-        dark = "mocha",
+        dark = "macchiato",
       },
       transparent_background = false,
       styles = {
