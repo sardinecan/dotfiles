5bcd6b56ad7743babadd848a59de2dbc36ce5766
Josselin Morvan <morvan.josselin@gmail.com>
2025-02-09 17:38:17 +0100

    Update .config/nvim/lua/plugins/onedarkpro.lua

--- private_dot_config/nvim/lua/plugins/onedarkpro.lua
+++ private_dot_config/nvim/lua/plugins/onedarkpro.lua
@@ -1,11 +1,28 @@
-return  {
-  "olimorris/onedarkpro.nvim",
-  priority = 1000, -- Ensure it loads first
-  opts = {
-    options = {
-      transparency = true,
-      highlight_inactive_windows = true
-    }
-  }
+return {
+  "navarasu/onedark.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("onedark").setup({
+      style = "darker", -- "dark", "darker", "cool", "deep", "warm", "warmer", "light"
+      transparent = false,
+      term_colors = true,
+
+      code_style = {
+        comments = "italic",
+        keywords = "bold",
+        functions = "italic",
+        variables = "NONE",
+        strings = "NONE",
+      },
+
+      -- lualine = { transparent = true }, -- Rend la barre de statut transparente
+      diagnostics = {
+        darker = true, -- couleurs plus sombres pour les diagnostics
+        undercurl = true, -- soulignements en courbe pour erreurs
+        background = true, -- fond pour les diagnostics
+      }
+    })
+  end,
 }
 
