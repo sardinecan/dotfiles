066b09be6660efab0e39e6381e01b835ee03d085
Josselin Morvan <morvan.josselin@gmail.com>
2025-03-03 09:00:13 +0100

    Update .config/nvim/lua/plugins/treesitter.lua

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return  {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
+    vim.treesitter.language.register('xml', { 'svg', 'xslt' })
     local config = require("nvim-treesitter.configs")
     config.setup({
       ensure_installed = {
@@ -18,7 +19,26 @@ return  {
       },
       auto_install = true,
       ignore_install = { "latex" },
-      highlight = { enable = true, disable = { "latex", "xml" }, },
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          -- Désactiver toujours pour LaTeX
+          if lang == "latex" then
+            return true
+          end
+
+          -- Désactiver pour les fichiers XML trop gros (> 800 KB)
+          if lang == "xml" then
+            local max_filesize = 800 * 1024 -- 800 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end
+
+          return false
+        end,
+      },
       indent = { enable = true },
       fold = { enable = true },
     })
