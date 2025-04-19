a95cde5d9a455ca7391881b07e8fd420dc762b5c
Josselin Morvan <morvan.josselin@gmail.com>
2025-04-19 16:05:01 +0200

    Update .config/nvim/lua/plugins/neotree.lua

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -7,13 +7,14 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    --[[require("neo-tree").setup({
+    require("neo-tree").setup({
+      close_if_last_window = true,
       window = {
         position = "right",
-        width = 25,  -- Définit la largeur de Neo-tree
+        width = 30,
       }
-    })]]
-    vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {}) -- open neotree panel
+    })
+    vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal right<CR>", {}) -- open neotree panel
     vim.keymap.set("n", "<leader>q", ":Neotree close<CR>", {}) -- close neotree panel
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {}) -- see opened files
     vim.keymap.set("n", "<leader>gst", ":Neotree git_status reveal float<CR>", {}) -- see git status files
