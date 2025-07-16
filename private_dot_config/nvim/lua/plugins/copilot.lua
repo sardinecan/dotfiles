5dae44414c0ad67b9436ede3e6f27d36d3f454cc
Josselin Morvan <morvan.josselin@gmail.com>
2025-07-16 11:49:08 +0200

    deactivated copilot for nvim by default

--- private_dot_config/nvim/lua/plugins/copilot.lua
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,17 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+  lazy = true,
+  cmd = { "Copilot", "Copilot enable", "Copilot disable", "Copilot status" },
+  keys = {
+    {
+      "<leader>cp",
+      "<cmd>Copilot status<CR>",
+      desc = "Activer GitHub Copilot"
+    },
+    {
+      "<leader>cP",
+      "<cmd>Copilot disable<CR>",
+      desc = "Désactiver GitHub Copilot"
+    },
+  },
 }
