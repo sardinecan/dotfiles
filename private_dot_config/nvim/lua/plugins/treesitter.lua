2566fdc900c1433b284415434f4579a082417586
Josselin Morvan <morvan.josselin@gmail.com>
2025-12-14 19:21:18 +0100

    Update .config/nvim/lua/plugins/treesitter.lua

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,46 +1,49 @@
-return  {
+return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    vim.treesitter.language.register('xml', { 'svg', 'xslt', "xhtml" })
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {
-        "css",
-        "html",
-        "javascript",
-        "julia",
-        "lua",
-        "markdown",
-        "python",
-        "vim",
-        "vimdoc",
-        "xml"
-      },
-      auto_install = true,
-      ignore_install = { "latex" },
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
-          -- Désactiver toujours pour LaTeX
-          if lang == "latex" then
+
+  -- exécuté AVANT le chargement du plugin
+  init = function()
+    vim.treesitter.language.register("xml", { "svg", "xslt", "xhtml" })
+  end,
+
+  opts = {
+    ensure_installed = {
+      "css",
+      "html",
+      "javascript",
+      "julia",
+      "lua",
+      "markdown",
+      "python",
+      "vim",
+      "vimdoc",
+      "xml",
+    },
+    auto_install = true,
+    ignore_install = { "latex" },
+
+    highlight = {
+      enable = true,
+      disable = function(lang, buf)
+        -- toujours désactivé pour LaTeX
+        if lang == "latex" then
+          return true
+        end
+
+        -- XML trop gros (> 800 KB)
+        if lang == "xml" then
+          local max_filesize = 800 * 1024
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
             return true
           end
+        end
 
-          -- Désactiver pour les fichiers XML trop gros (> 800 KB)
-          if lang == "xml" then
-            local max_filesize = 800 * 1024 -- 800 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end
+        return false
+      end,
+    },
 
-          return false
-        end,
-      },
-      indent = { enable = true },
-      fold = { enable = true },
-    })
-  end
+    indent = { enable = true },
+  },
 }
