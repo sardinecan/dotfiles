93cfeefa3c5261585ec4b0eebd60c4cb6a5c1c50
Josselin Morvan <morvan.josselin@gmail.com>
2025-12-17 16:30:48 +0100

    Update .config/nvim/ftplugin/julia.lua

--- private_dot_config/nvim/ftplugin/julia.lua
+++ private_dot_config/nvim/ftplugin/julia.lua
@@ -2,4 +2,5 @@
 vim.bo.tabstop = 2      -- 1 tab = 2 espaces
 vim.bo.shiftwidth = 2   -- 2 espaces pour l'indentation automatique
 vim.bo.expandtab = true -- convertir les tabs en espaces
+vim.wo.wrap = false
 
