76b61316f3790565bfb58d1dc576b4d3b7c95fff
Josselin Morvan <morvan.josselin@gmail.com>
2025-12-20 15:20:46 +0100

    Update .config/nvim/lua/plugins/treesitter.lua

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,48 +2,57 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
-  -- exécuté AVANT le chargement du plugin
   init = function()
     vim.treesitter.language.register("xml", { "svg", "xslt", "xhtml" })
   end,
 
-  opts = {
-    ensure_installed = {
-      "css",
-      "html",
-      "javascript",
-      "julia",
-      "lua",
-      "markdown",
-      "python",
-      "vim",
-      "vimdoc",
-      "xml",
-    },
-    auto_install = true,
-    ignore_install = { "latex" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "css",
+        "html",
+        "javascript",
+        "julia",
+        "lua",
+        "markdown",
+        "python",
+        "vim",
+        "vimdoc",
+        "xml",
+      },
+      auto_install = true,
+      ignore_install = { "latex" },
 
-    highlight = {
-      enable = true,
-      disable = function(lang, buf)
-        -- toujours désactivé pour LaTeX
-        if lang == "latex" then
-          return true
-        end
-
-        -- XML trop gros (> 800 KB)
-        if lang == "xml" then
-          local max_filesize = 800 * 1024
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          if lang == "latex" then
             return true
           end
-        end
 
-        return false
+          if lang == "xml" then
+            local max_filesize = 800 * 1024
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end
+
+          return false
+        end,
+      },
+
+      indent = { enable = true },
+    })
+
+    -- Force Treesitter pour Julia
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "julia",
+      callback = function()
+        vim.schedule(function()
+          vim.cmd("TSBufEnable highlight")
+        end)
       end,
-    },
-
-    indent = { enable = true },
-  },
+    })
+  end,
 }
